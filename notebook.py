05700ba6dfaead9a217f48a3ebb540b309339a4f
ctr26 <ctr26@ebi.ac.uk>
2022-05-13 11:36:40 +0100

    plots plots plots

---
+++ notebook.py
@@ -0,0 +1,414 @@
+# %%
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier, BaggingClassifier
+from sklearn.feature_selection import SelectFromModel, RFECV, RFE
+from sklearn.decomposition import PCA
+from sklearn.pipeline import Pipeline
+from sklearn import metrics
+from sklearn import model_selection
+import pathlib
+
+sns.set()
+from cellesce import Cellesce
+
+VARIABLES = ["Conc /uM", "Date", "Drug"]
+SAVE_FIG = True
+SAVE_CSV = True
+
+# data_folder = "analysed/210720 - ISO49+34 - projection_XY/unet_2022/project_XY_all"
+# data_folder = "analysed/210720 - ISO49+34 - projection_XY/unet_2022/project_XY_objects"
+
+pd.read_csv("analysed/_2019_cellesce_unet_splineparameters_aligned/raw/projection_XY/Secondary.csv")
+
+kwargs = {
+    "data_folder": "analysed/210720 - ISO49+34 - projection_XY/unet_2022/project_XY_all",
+    "nuclei_path": "object_filteredNuclei.csv", 
+}
+kwargs = {
+    "data_folder": "analysed/210720 - ISO49+34 - projection_XY/unet_2022/project_XY_objects",
+    "nuclei_path": "Secondary.csv",
+}
+
+
+kwargs={
+    "data_folder": "analysed/_2019_cellesce_unet_splineparameters_aligned/raw/projection_XY/",
+    "nuclei_path": "Secondary.csv"
+}
+
+
+kwargs={
+    "data_folder": "analysed/_2019_cellesce_unet_splineparameters_aligned/raw/projection_XY/",
+    "nuclei_path": "Secondary.csv"
+}
+
+kwargs = {
+    "data_folder": "analysed/210720 - ISO49+34 - projection_XY/unet_2022/project_XY_objects",
+    "nuclei_path": "object_filteredNuclei.csv",
+}
+
+results_folder = f'{kwargs["data_folder"]}/results'
+pathlib.Path(results_folder).mkdir(parents=True, exist_ok=True)
+
+def metadata(x):
+    path = pathlib.Path(results_folder,x)
+    print(path)
+    return path
+# %%
+
+
+from cellesce import Cellesce
+df = Cellesce(**kwargs).get_data().cellesce.clean().cellesce.preprocess()
+
+
+print(
+    f'Organoids: {df.cellesce.grouped_median("ObjectNumber").cellesce.simple_counts()}',
+    f"Nuclei: {df.cellesce.simple_counts()}",
+)
+
+# %%
+
+# sns.barplot(
+#     y="Feature", x="Importance",
+#     data=df.cellesce.grouped_median().cellesce.feature_importances(variable="Cell").reset_index()
+# )
+# plt.tight_layout()   
+
+
+# importance = df.cellesce.feature_importances(variable="Cell").reset_index()
+
+sns.barplot(
+    y="Feature", x="Importance",
+    data=df.cellesce.feature_importances(variable="Cell").reset_index()
+)
+
+sns.barplot(
+    y="Feature", x="Importance",
+    data=df.cellesce.grouped_median().cellesce.feature_importances(variable="Cell").reset_index()
+)
+
+# sns.barplot(
+#     y="Feature", x="Cumulative Importance",
+#     data=df.cellesce.feature_importances(variable="Cell").reset_index()
+# )
+# plt.tight_layout()   
+
+
+# %% Could do better with median per imagenumber
+plot = sns.catplot(
+    x="Kind",
+    y="Score",
+    col="Metric",
+    # row="Cell",
+    ci=None,
+    hue="Population type",
+    data=pd.concat(
+        [
+            (
+                df.cellesce.get_score_report("Cell").assign(
+                    **{"Population type": "Nuclei"}
+                )
+            ),
+            (
+                df.cellesce.grouped_median("ObjectNumber")
+                .cellesce.get_score_report("Cell")
+                .assign(**{"Population type": "Organoid"})
+            ),
+        ]
+    ),
+    sharey=False,
+    kind="bar",
+    col_wrap=2,
+).set_xticklabels(rotation=45)
+if SAVE_FIG: plt.savefig(metadata("Cell_predictions_image_vs_nuclei.pdf"))
+plt.show()
+# %%
+plot = sns.catplot(
+    # x="Kind",
+    y="Score",
+    col="Metric",
+    x="Cell",
+    ci=None,
+    hue="Kind",
+    data=(
+        df.cellesce.grouped_median("ObjectNumber")
+        .groupby(level="Cell")
+        .apply(lambda x: x.cellesce.get_score_report("Drug"))
+        .reset_index()
+    ),
+    sharey=False,
+    kind="bar",
+    col_wrap=2,
+).set_xticklabels(rotation=45)
+if SAVE_FIG: plt.savefig(metadata("Drug_predictions_per_organoid.pdf"))
+plt.show()
+# %%
+sns.catplot(
+    y="Conc /uM",
+    hue="Drug",
+    x="Organoids",
+    col="Cell",
+    sharex=True,
+    kind="bar",
+    orient="h",
+    ci=None,
+    data=(
+        df
+        #   .grouped_median("ObjectNumber")
+        .cellesce.groupby_counts("ImageNumber")
+    ).reset_index(name="Organoids"),
+)
+if SAVE_FIG: plt.savefig(metadata("Organoid_Summary.pdf"))
+plt.show()
+# %%
+sns.catplot(
+    y="Conc /uM",
+    hue="Drug",
+    x="Nuclei",
+    col="Cell",
+    sharex=True,
+    kind="bar",
+    orient="h",
+    ci=None,
+    data=(df.cellesce.groupby_counts("ObjectNumber"))
+    .reset_index(name="Nuclei"),
+)
+if SAVE_FIG: plt.savefig(metadata("Nuclei_Summary.pdf"))
+plt.show()
+# %%
+plot = sns.histplot(
+    x="Date",
+    weights="Organoids",
+    hue="Drug",
+    data=(
+        df.cellesce.grouped_median("ObjectNumber")
+        .cellesce.groupby_counts("ImageNumber")
+        .reset_index(name="Organoids")
+    ),
+    multiple="stack",
+)
+plt.xticks(rotation=90)
+plt.tight_layout()
+if SAVE_FIG: plt.savefig(metadata("Date_summary_organoids.pdf"))
+plt.show()
+# %%
+
+plot = sns.histplot(
+    x="Date",
+    weights="Organoids",
+    hue="Drug",
+    data=(
+        df.cellesce.grouped_median("ObjectNumber")
+        .cellesce.groupby_counts("ImageNumber")
+        .reset_index(name="Organoids")
+    ),
+    multiple="stack",
+)
+plt.xticks(rotation=90)
+plt.tight_layout()
+if SAVE_FIG: plt.savefig(metadata("Date_summary_organoids.pdf"))
+plt.show()
+# %%
+# sns.clustermap(
+#     (
+#         df.cellesce.grouped_median("ObjectNumber")
+#         .cellesce.keeplevel(["Cell", "Drug"])
+#         .T.corr()
+#     )
+# )
+# plt.show()
+# sns.clustermap(
+#     (df.cellesce.grouped_median("ObjectNumber")
+#      .cellesce.keeplevel(["Cell"])
+#      .T.corr())
+# )
+# plt.show()
+# sns.clustermap(
+#     (df.cellesce.grouped_median("ObjectNumber")
+#      .cellesce.keeplevel(["Drug"])
+#      .T.corr())
+# )
+# plt.show()
+
+
+# # %%
+# # model = Pipeline([
+# #                     ("PCA", PCA()),
+# #                     ("modelselect", SelectFromModel(RandomForestClassifier())),
+# #                     ("RandomForest",RandomForestClassifier())
+# #                   ])
+# model = RandomForestClassifier()
+# # model = Pipeline([
+# #                     ("PCA", PCA()),
+# #                     ("RandomForest",RandomForestClassifier())
+# #                   ])
+
+# importance = df.cellesce.feature_importances(RandomForestClassifier(), variable="Cell")
+# print("Nuclei Cell")
+# importance = df.cellesce.grouped_median().cellesce.feature_importances(
+#     model, variable="Cell"
+# )
+# print("Organoid Cell")
+
+# importance = df.cellesce.feature_importances(RandomForestClassifier(), variable="Drug")
+# print("Nuclei Drug")
+
+# importance = df.cellesce.grouped_median().cellesce.feature_importances(model, variable="Drug")
+# print("Organoid Drug")
+
+
+
+# # fig_dims = (8, 6)
+
+# sns.barplot(
+#     y="Feature", x="Cumlative importance",
+#     data=df.cellesce.feature_importances(variable="Cell")
+# )
+# plt.tight_layout()
+
+
+    
+    
+# %%
+df_new = df.cellesce.select_features().cellesce.grouped_median("ObjectNumber")
+# importance = df.classification_report(model)
+# # %%
+# VARS = ["Cell", "Drug", "Conc /uM"]
+# variable = VARS[1]
+# # for VAR in VARS:
+# def get_score_report(df, variable="Cell"):
+#     # labels, uniques = pd.factorize(df.reset_index()[variable])
+#     X, y = df, list(df.index.get_level_values(variable))
+#     uniques = df.index.get_level_values(variable).to_series().unique()
+#     X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y)
+#     X_train, X_test, y_train, y_test = df.cellesce.train_test_split(variable)
+#     model.fit(X_train, y_train)
+#     # y_pred = pd.Series(model.predict(X_test), index=X_test.index)
+
+#     report = pd.DataFrame(
+#         metrics.classification_report(y_test, model.predict(X_test), output_dict=True)
+#     ).drop(["accuracy", "macro avg", "weighted avg"], axis=1)
+#     report_tall = (
+#         report.rename_axis("Metric")
+#         .melt(
+#             # id_vars="Metric",
+#             var_name="Kind",
+#             value_name="Score",
+#             ignore_index=False,
+#         )
+#         .assign(**{"Variable": variable})
+#         .reset_index()
+#     )
+#     report_tall["Cohen Kappa"] = metrics.cohen_kappa_score(
+#         y_test, model.predict(X_test)
+#     )
+#     return report_tall
+
+
+# %%
+# data = report_df.set_index("Variable").xs("Drug")
+
+# %%
+plot = sns.catplot(
+    x="Kind",
+    y="Score",
+    col="Metric",
+    # row="Cell",
+    ci=None,
+    # hue="Metric",
+    data=(
+        df.cellesce.grouped_median("ObjectNumber").cellesce.get_score_report("Drug")
+        .reset_index()
+    ),
+    sharey=False,
+    kind="bar",
+).set_xticklabels(rotation=45)
+plt.show()
+
+# %%
+# data=pd.concat([
+#         get_score_report(df.cellesce.grouped_median("ObjectNumber"),"Cell").assign(**{"Population type": "Organoid"}),
+#         get_score_report(df,"Cell").assign(**{"Population type": "Nuclei"})
+#         ]),
+
+# df.cellesce.grouped_median("ObjectNumber"),"Cell").groupby(level="Cell").apply(get_score_report)
+# plot = sns.catplot(
+#     x="Kind",
+#     y="Score",
+#     col="Metric",
+#     # row="Cell",
+#     ci=None,
+#     # hue="Metric",
+#     data=report_df.xs("Drug", level="Variable").reset_index(),
+#     sharey=False,
+#     kind="bar",
+# ).set_xticklabels(rotation=45)
+# %%
+
+
+# # a.cellesce.test_fun()
+
+# a.groupby("col1").apply(lambda d: d.cellesce.test_fun())
+
+# %%
+
+# df.pipe(get_score_report,variable)
+# (df
+#  .groupby(level=["Conc /uM"])
+#  .apply(get_score_report,)
+# )
+
+# report_df = [df.groupby(level=variable).apply(
+#     lambda x: get_score_report(x,variable)
+#     )
+#  for variable in [VARS[1]]];report_df
+
+# report_df = [df.groupby(level=variable).apply(
+#     lambda x: get_score_report(x,variable)
+#     )
+#  for variable in [VARS[0]]];report_df
+
+# report_df = [df.groupby(level=variable).apply(
+#     lambda x: get_score_report(x,variable)
+#     )
+#  for variable in [VARS[2]]];report_df
+# %%
+# return report_tall
+# a = get_score_report(df,"Cell")
+# b = get_score_report(df,"Drug")
+# c = get_score_report(df,"Conc /uM")
+# report_df = pd.concat(
+#     [
+#         get_score_report(df.cellesce.grouped_median("ObjectNumber"), variable).assign(
+#             **{"Population type": "Median"}
+#         )
+#         for variable in VARS
+#     ]
+# ).set_index(["Metric", "Kind", "Variable", "Population type"])
+
+# report_df = pd.concat(
+#         get_score_report(df.cellesce.grouped_median("ObjectNumber"), "Cell").assign(
+#             **{"Population type": "Median"}
+#         )
+# ).set_index(["Metric", "Kind", "Variable", "Population type"])
+# %%
+
+# df.cellesce.grouped_median("ObjectNumber").apply()
+
+# (pd.concat([
+#     df.assign(**{"Population type": "Organoid"}),
+#     df.cellesce.grouped_median("ObjectNumber")
+#     ])
+#     .apply(lambda x: get_score_report(x,"Cell"))
+# )
+
+# df.cellesce.grouped_median("ObjectNumber").apply(lambda x: get_score_report(x,"Cell"))
+
+# pd.concat([
+# get_score_report(df.cellesce.grouped_median("ObjectNumber"),"Cell").assign(**{"Population type": "Organoid"}),
+# get_score_report(df,"Cell").assign(**{"Population type": "Nuclei"})
+# ])
+# get_score_report(df, "Cell").assign(**{"Population type": "Median"})
+# get_score_report(df.cellesce.grouped_median("ObjectNumber"), "Cell")
